44964b059114da8357e93ad5319d537739cc1a54
Gabriel Topciu <gb.topciu@gmail.com>
2022-02-04 14:47:58 +0000

    dasdas

--- src/components/TodoPosts.js
+++ src/components/TodoPosts.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TodoPosts = ({ todoPosts, name, todoPost, setTodoPosts }) => {
-  const deleteRedditPostHandler = () => {
+  const deleteTodoPostHandler = () => {
     setTodoPosts(todoPosts.filter((t) => t !== todoPost));
   };
 
@@ -10,7 +10,7 @@ const TodoPosts = ({ todoPosts, name, todoPost, setTodoPosts }) => {
       <h2 className="text-5xl font-bold mb-2">{name} Posts</h2>
       <h3 className="text-3xl mb-2">{todoPost}</h3>
       <button
-        onClick={deleteRedditPostHandler}
+        onClick={deleteTodoPostHandler}
         className="justify-center text-3xl bg-red-600 rounded text-white p-2"
       >
         Delete
